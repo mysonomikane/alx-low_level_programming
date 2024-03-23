5a3e739dc0031b5012f9f791daec27574b4c37e5
Abderrahman MOULAY ALI FRATT <mou.ali.abd@gmail.com>
2024-03-23 04:01:16 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * leet - encode a string into 1337
+ * @s: string
+ * Return: encoded string `s`
+ */
+
+char *leet(char *s)
+{
+	char *cp = s;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+	unsigned int i;
+
+	while (*s)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+			/*32 is the difference between lower case letters and apper case letters*/
+			if (*s == key[i] || *s == key[i] + 32)
+			{
+				*s = 48 + value[i];
+			}
+		}
+		s++;
+	}
+
+	return (cp);
+
+}
